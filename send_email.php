48ca6450575aaee79776d15bec469192a620122d
Christian Romero <khreez@gmail.com>
2010-09-27 20:19:53 -0500

    se incluyen los cambios que estan en el server

--- send_email.php
+++ send_email.php
@@ -9,10 +9,10 @@ if( $contact_name == true )
 	$sender = $contact_email;
 	$receiver = "syra@infinitum.com.mx";
 	$client_ip = $_SERVER['REMOTE_ADDR'];
-	$email_body = "Name: $contact_name \nEmail: $sender \nSubject: $contact_subject \nMessage: $contact_message";		
+	$email_body = "Name: $contact_name \nEmail: $sender \nSubject: $contact_subject \nMessage: $contact_message \nIP: $client_ip \nFlash Contact Form provided by http://www.flashmo.com";		
 	$extra = "From: $sender\r\n" . "Reply-To: $sender \r\n" . "X-Mailer: PHP/" . phpversion();
 
-	if( mail( $receiver, "Mensaje desde tienda syra - $subject", $email_body, $extra ) ) 
+	if( mail( $receiver, "Flash Contact Form - $subject", $email_body, $extra ) ) 
 	{
 		echo "success=yes";
 	}
