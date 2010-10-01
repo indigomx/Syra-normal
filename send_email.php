0cc512f2e9782bc9a4b55e7fc0190087dfe7fb9f
Christian Romero <khreez@gmail.com>
2010-10-01 11:29:05 -0500

    modificando los parametros del envio de correos

--- send_email.php
+++ send_email.php
@@ -7,12 +7,12 @@ $contact_message = $_POST['message'];
 if( $contact_name == true )
 {
 	$sender = $contact_email;
-	$receiver = "syra@infinitum.com.mx";
+	$receiver = "christian@dsindigo.com";
 	$client_ip = $_SERVER['REMOTE_ADDR'];
-	$email_body = "Name: $contact_name \nEmail: $sender \nSubject: $contact_subject \nMessage: $contact_message \nIP: $client_ip \nFlash Contact Form provided by http://www.flashmo.com";		
+	$email_body = "Name: $contact_name \nEmail: $sender \nSubject: $contact_subject \nMessage: $contact_message \nIP: $client_ip \n";		
 	$extra = "From: $sender\r\n" . "Reply-To: $sender \r\n" . "X-Mailer: PHP/" . phpversion();
 
-	if( mail( $receiver, "Flash Contact Form - $subject", $email_body, $extra ) ) 
+	if( mail( $receiver, "Contacto Syra - $contact_subject", $email_body, $extra ) ) 
 	{
 		echo "success=yes";
 	}
